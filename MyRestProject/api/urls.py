7ad57ee43ce7f6d24851dcd55c199646b61a1dd8
marcin <test@gmail.com>
2021-02-28 17:51:30 +0100

    Added info about new endpoint

--- MyRestProject/api/urls.py
+++ MyRestProject/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import car_list
+from .views import car_list, car_detail
 
 urlpatterns = [
-    path('car/', car_list)
+    path('car/', car_list),
+    path('detail/<int:pk>/', car_detail)
 ]
