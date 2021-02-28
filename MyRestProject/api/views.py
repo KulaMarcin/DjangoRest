7b848f729b89192b465744c70c5d9abf1cf45be5
marcin <test@gmail.com>
2021-02-28 12:48:40 +0100

    Created view with GET and POST for car

--- MyRestProject/api/views.py
+++ MyRestProject/api/views.py
@@ -1,3 +1,24 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.parsers import JSONParser
+
+from MyRestProject.api.models import Car
+from MyRestProject.api.serializers import CarSerializer
+
+
+def car_list(request):
+
+    if request == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CarSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
