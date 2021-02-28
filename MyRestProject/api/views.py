c46a5330e1783c26d569662eeb6e8310eb87f6d5
marcin <test@gmail.com>
2021-02-28 19:06:16 +0100

    Added generic view to get, post put and delete on Person

--- MyRestProject/api/views.py
+++ MyRestProject/api/views.py
@@ -8,11 +8,43 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
 
-from .models import Car
-from .serializers import CarSerializer
+from .models import Car, Person
+from .serializers import CarSerializer, PersonSerializer
 
-# API_VIEW BASED CLASS
+# GENERIC VIEW BASED CLASS FOR PERSON - VERY FAST FOR CRUD
+
+
+class GenericAPIView(generics.GenericAPIView,
+                     mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin):
+
+    serializer_class = PersonSerializer
+    queryset = Person.objects.all()
+
+    lookup_field = 'id'
+
+    def get(self, request, id = None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
+
+# API_VIEW BASED CLASS FOR CAR
 
 
 class CarAPIView(APIView):
