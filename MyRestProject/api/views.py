31ed94861a72ea7b1e0619f8ce6cc3700f86e332
marcin <test@gmail.com>
2021-02-28 17:51:19 +0100

    Added method for PUT, DELETE and GET with pk param

--- MyRestProject/api/views.py
+++ MyRestProject/api/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -24,3 +24,29 @@ def car_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def car_detail(request, pk):
+    try:
+        car = Car.objects.get(pk=pk)
+
+    except Car.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = CarSerializer(car)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = CarSerializer(car, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        car.delete()
+        return HttpResponse(status=204)
