f9554261783abb43312fe83a633f749c0f2bf627
marcin <test@gmail.com>
2021-02-28 12:23:47 +0100

    Added Car model to models.py

--- MyRestProject/api/models.py
+++ MyRestProject/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    id = models.IntegerField
+    manufacturer = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    color = models.CharField(max_length=20)
+    engine_type = models.CharField(max_length=20)
+    price = models.IntegerField
+
+    def __str__(self):
+        return self.manufacturer
